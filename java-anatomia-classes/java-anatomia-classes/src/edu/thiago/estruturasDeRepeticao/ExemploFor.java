2f7906902b3bd559433e5c45572b01749c56754c
Thiago <thiagoesmael19@gmail.com>
2024-10-05 09:49:42 -0300

    Feat: Adicionando estruturas de repetição

---
+++ java-anatomia-classes/java-anatomia-classes/src/edu/thiago/estruturasDeRepeticao/ExemploFor.java
@@ -0,0 +1,12 @@
+package edu.thiago.estruturasDeRepeticao;
+
+public class ExemploFor {
+    public static void main(String[] args) {
+        System.out.println("Contando Carneirinhos");
+        for(int carneirinhos = 1; carneirinhos <= 20; carneirinhos++){
+            System.out.println(carneirinhos +" Carneirinhos");
+        }
+        System.out.println("Joãozinho dormiu");
+    }
+    
+}
